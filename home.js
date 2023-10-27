0d12aa5ca4b7b1f47285e796bf66b6a01b01e1c3
Musaddik Khan <imusaddikkhan@gmail.com>
2023-10-28 00:14:19 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,13 @@
+const popupBtn = document.querySelector("#Eth")
+const PopUp = document.querySelector("#popUp")
+console.log(popupBtn, PopUp)
+
+popupBtn.addEventListener("click", () => {
+   PopUp.classList.toggle("open-menu")
+   setTimeout(() => {
+    PopUp.classList.remove("open-menu")
+   }, 1500);
+})
+
+
+
